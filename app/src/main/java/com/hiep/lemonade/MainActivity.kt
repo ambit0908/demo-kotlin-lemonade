52fae6999ec146bc4f551b2d55a1ffef9f9391f6
TRIEU QUANG HIEP <ambit.0908@gmail.com>
2025-04-18 11:56:16 +0900

    Lemonade app

---
+++ app/src/main/java/com/hiep/lemonade/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.hiep.lemonade
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.hiep.lemonade.ui.theme.LemonadeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LemonadeTheme {
+                LemonApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun LemonApp() {
+    var step: Int by remember { mutableIntStateOf(1) }
+    if (step > 4) step = 1
+    val imageSource = when (step) {
+        1 -> R.drawable.lemon_tree
+        2 -> R.drawable.lemon_squeeze
+        3 -> R.drawable.lemon_drink
+        else -> R.drawable.lemon_restart
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        GreetingImage(imageSource, onImageClick = { step++ })
+
+        Spacer(Modifier.height(32.dp))
+
+        val text: String = when (step) {
+            1 -> "Tap the lemon tree to select a lemon"
+            2 -> "Keep tapping the lemon to squeeze it"
+            3 -> "Tap the lemonade to drink it"
+            else -> "Tap the empty glass to start again"
+        }
+
+        Text(
+            text = text
+        )
+    }
+}
+
+
+@Composable
+fun GreetingImage(imageSource: Int, onImageClick: () -> Unit) {
+    Image(
+        painter = painterResource(imageSource),
+        contentDescription = stringResource(imageSource),
+        Modifier
+            .clip(RoundedCornerShape(32.dp))
+            .background(Color(0xFFC3ECD2))
+            .width(200.dp)
+            .height(200.dp)
+            .padding(16.dp)
+            .clickable { onImageClick() },
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LemonadeTheme {
+        LemonApp()
+    }
+}
