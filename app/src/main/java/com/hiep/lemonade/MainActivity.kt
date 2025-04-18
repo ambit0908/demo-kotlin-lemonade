37653999fb905e6ad8891b1c7c66c0c1e6145c37
TRIEU QUANG HIEP <ambit.0908@gmail.com>
2025-04-18 13:57:48 +0900

    add status bar padding

--- app/src/main/java/com/hiep/lemonade/MainActivity.kt
+++ app/src/main/java/com/hiep/lemonade/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
@@ -62,6 +63,7 @@ fun LemonApp() {
         text = stringResource(R.string.app_name),
         modifier = Modifier
             .fillMaxWidth()
+            .statusBarsPadding()
             .background(color = Color.Yellow)
             .height(100.dp),
         textAlign = TextAlign.Center,
